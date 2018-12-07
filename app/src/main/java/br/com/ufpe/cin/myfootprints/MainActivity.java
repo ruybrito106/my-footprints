ce358dda0f32abc3762bcbfb0e57524a39472d9d
Higor Cavalcanti <higor.cavalcanti@inlocomedia.com>
2018-12-06 22:24:39 -0300

    feat/implementing google maps integration

--- app/src/main/java/br/com/ufpe/cin/myfootprints/MainActivity.java
+++ app/src/main/java/br/com/ufpe/cin/myfootprints/MainActivity.java
@@ -2,21 +2,22 @@ package br.com.ufpe.cin.myfootprints;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TimePicker;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.DateFormat;
@@ -28,7 +29,7 @@ import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 
-public class MainActivity extends AppCompatActivity implements OnDateSetListener {
+public class MainActivity extends AppCompatActivity implements OnDateSetListener, OnMapReadyCallback {
 
     private static final int PERMISSIONS_REQUEST_CODE = 200;
     private static final String[] PERMISSIONS = {
@@ -36,11 +37,13 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
             ACCESS_COARSE_LOCATION,
     };
 
-    private TextView mTextMessage;
     private String phoneNumber;
-    private EditText startDate;
-    private EditText endDate;
+    private EditText startDateText;
+    private EditText endDateText;
+    private Date startDate;
+    private Date endDate;
     private DateFormat dateFormat;
+    private GoogleMap mMap;
 
 
     private LocationUpdateDAO dbInstance;
@@ -52,10 +55,8 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
             }
             return false;
@@ -81,47 +82,38 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
 
     @Override
     public void onDateSet(String type, int year, int month, int day) {
+        Date date  = new Date(year, month, day);
+        DateHelper helper = new DateHelper();
         if(type.equals("START")){
-            startDate.setText(dateFormat.format(new Date(year, month, day)));
+            startDateText.setText(dateFormat.format(date));
+            startDate = helper.atStartOfDay(date);
         } else {
-            endDate.setText(dateFormat.format(new Date(year, month, day)));
+            endDateText.setText(dateFormat.format(date));
+            endDate = helper.atEndOfDay(date);
         }
     }
 
     private void populateView() {
 
-        startDate = (EditText) findViewById(R.id.startDate);
-        endDate = (EditText) findViewById(R.id.endDate);
+        startDateText = (EditText) findViewById(R.id.startDate);
+        endDateText = (EditText) findViewById(R.id.endDate);
 
-        startDate.setText(dateFormat.format(Calendar.getInstance().getTime()));
-        startDate.setInputType(InputType.TYPE_NULL);
-        endDate.setText(dateFormat.format(Calendar.getInstance().getTime()));
-        endDate.setInputType(InputType.TYPE_NULL);
+        startDateText.setText(dateFormat.format(Calendar.getInstance().getTime()));
+        startDateText.setInputType(InputType.TYPE_NULL);
+        endDateText.setText(dateFormat.format(Calendar.getInstance().getTime()));
+        endDateText.setInputType(InputType.TYPE_NULL);
 
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        final TextView pathText = (TextView) findViewById(R.id.routeText);
-        pathText.setMovementMethod(new ScrollingMovementMethod());
 
         Button pathButton = (Button) findViewById(R.id.findRoute);
 
         pathButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Date current = new Date();
-                DateHelper helper = new DateHelper();
+                updateMap();
 
-                List<LocationUpdate> path = getLocationUpdatesByTimeRange(helper.atStartOfDay(current), helper.atEndOfDay(current));
-                String pathStr = "";
-
-                for (int i = 0; i < path.size(); i++) {
-                    pathStr += path.get(i).toString() + " ";
-                }
-
-                pathText.setText(pathStr);
             }
         });
 
@@ -136,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
     public void showEndDatePickerDialog(View v) {
         DatePickerFragment newFragment = new DatePickerFragment();
         newFragment.setOnDateSetListener(this, "END");
-        newFragment.show(getSupportFragmentManager(), "startDatePicker");
+        newFragment.show(getSupportFragmentManager(), "endDatePicker");
     }
 
 
@@ -145,8 +137,20 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dateFormat =android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
         phoneNumber = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
+
+        Date current = new Date();
+        DateHelper helper = new DateHelper();
+
+        startDate = helper.atStartOfDay(current);
+        endDate = helper.atEndOfDay(current);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         if (!canAccessLocation()) {
             this.requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
@@ -157,5 +161,22 @@ public class MainActivity extends AppCompatActivity implements OnDateSetListener
 
         dbInstance = LocationUpdateDAO.getInstance(this);
         populateView();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        updateMap();
+    }
+
+    void updateMap(){
+        mMap.clear();
+        List<LocationUpdate> path = getLocationUpdatesByTimeRange(startDate, endDate);
+        for(LocationUpdate location : path){
+            LatLng latlng = new LatLng(location.getLat(), location.getLng());
+            String label = dateFormat.format(new Date((long)location.getTimestampSeconds()*1000));
+            mMap.addMarker(new MarkerOptions().position(latlng).title(label));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        }
     }
 }
